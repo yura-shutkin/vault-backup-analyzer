b6f0e6023e01db965ae84de4be2719160665b252
Shutkin Yura <shutkin.yurii@gmail.com>
2019-08-12 12:32:59 +0500

    Add approle and yaml object unification

--- local-env/seed.py
+++ local-env/seed.py
@@ -6,7 +6,8 @@ def enable_auth_backends(session, backends):
     for top_key in backends:
         for idx in range(len(backends[top_key])):
             session.sys.enable_auth_method(method_type=backends[top_key][idx]['method_type'],
-                                           path=backends[top_key][idx]['path'], config=backends[top_key][idx]['config'])
+                                           path=backends[top_key][idx]['mount_point'],
+                                           config=backends[top_key][idx]['config'])
 
 
 def create_policies(session, policies):
@@ -22,10 +23,20 @@ def create_userpass_users(session, userpass_users):
     for top_key in userpass_users:
         for idx in range(len(userpass_users[top_key])):
             session.create_userpass(
-                username=userpass_users[top_key][idx]['username'],
+                username=userpass_users[top_key][idx]['name'],
                 password=userpass_users[top_key][idx]['password'],
                 policies=userpass_users[top_key][idx]['policies'],
                 mount_point=userpass_users[top_key][idx]['mount_point']
+            )
+
+
+def create_approles(session, approles):
+    for top_key in approles:
+        for idx in range(len(approles)):
+            session.create_role(
+                role_name=approles[top_key][idx]['name'],
+                mount_point=approles[top_key][idx]['mount_point'],
+                param=approles[top_key][idx]['params']
             )
 
 
@@ -41,8 +52,11 @@ if __name__ == '__main__':
     AUTH_BACKENDS = open_yaml('seed_configs/auth_backends.yml')
     POLICIES = open_yaml('seed_configs/policies.yml')
     USERPASS_USERS = open_yaml('seed_configs/userpass_users.yml')
+    APPROLES = open_yaml('seed_configs/approles.yml')
+
     ROOT_SESSION = hvac.Client(url=VAULT_ADDR, token=ROOT_TOKEN)
 
     enable_auth_backends(ROOT_SESSION, AUTH_BACKENDS)
     create_policies(ROOT_SESSION, POLICIES)
     create_userpass_users(ROOT_SESSION, USERPASS_USERS)
+    create_approles(ROOT_SESSION, APPROLES)
